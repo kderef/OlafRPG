8abdd23ad11e73a93dd2741df4c6518a112898ed
kvoid <kianheitkamp@outlook.com>
2024-07-11 22:49:15 +0200

    RESET

--- src/core/embed.c
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#define INCBIN_STYLE INCBIN_STYLE_SNAKE
-#define INCBIN_PREFIX bin_
-#include "incbin.h"
-
-#define embed INCBIN
--- src/game/state.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-typedef enum GameState {
-    GAME_MENU = 0,
-    GAME_RUNNING,
-} GameState;
